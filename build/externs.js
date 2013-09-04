a89d4a7a57424d1fd454cb556a82c3f34bfaa4cc
mone <simone.fabiano@gmail.com>
2013-09-04 17:52:37 +0200

    introduce Inheritance externs

--- build/externs.js
+++ build/externs.js
@@ -6,3 +6,8 @@ Executor.addTimedTask = function() {};
 
 Helpers = {};
 Helpers.isArray =  function() {};
+
+Inheritance =  function() {};
+Inheritance._super_ =  {};
+Inheritance._callSuperConstructor =  function() {};
+Inheritance._callSuperMethod =  function() {};
